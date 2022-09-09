7a35fb722a944197a568a4dee7ffa167771f5e3b
Baha Tastan <baha@cybertekschool.com>
2022-09-09 21:47:13 +0100

    Initial commit

---
+++ src/main/java/com/cydeo/entity/BaseEntity.java
@@ -0,0 +1,14 @@
+package org.cydeo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public abstract class BaseEntity {
+    public int id;
+}
