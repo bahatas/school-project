88cc2f492ceb22e4800d07aab8bb03f750ca5039
Kramer Brown <brownkramer63@gmail.com>
2022-09-11 18:34:28 -0500

    schoolservice user story

--- src/main/java/com/cydeo/service/SchoolService.java
+++ src/main/java/com/cydeo/service/SchoolService.java
@@ -1,17 +1,21 @@
 package com.cydeo.service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import com.cydeo.database.Database;
 public class SchoolService implements CRUDService{
     @Override
     public Object findById(int id) {
-        return null;
+        return Database.schoolList.stream()
+                .filter(school -> school.getId()==(id));
         //comment
     }
 
     @Override
     public List findAll() {
-        return null;
+        return Database.schoolList.stream().filter(school -> school.getName()!="").collect(Collectors.toList());
+        //in progress
     }
 
     @Override
@@ -26,6 +30,6 @@ public class SchoolService implements CRUDService{
 
     @Override
     public void deleteById(Long id) {
-
+Database.schoolList.remove(id);
     }
 }
