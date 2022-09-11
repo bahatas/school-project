6af76c9bb70352bb05d27541753e46a0aec17045
Kramer Brown <brownkramer63@gmail.com>
2022-09-11 16:28:00 -0500

    schoolservice user story

---
+++ src/main/java/com/cydeo/service/SchoolService.java
@@ -0,0 +1,30 @@
+package com.cydeo.service;
+
+import java.util.List;
+
+public class SchoolService implements CRUDService{
+    @Override
+    public Object findById(int id) {
+        return null;
+    }
+
+    @Override
+    public List findAll() {
+        return null;
+    }
+
+    @Override
+    public void save(Object o) {
+
+    }
+
+    @Override
+    public void update(Object o) {
+
+    }
+
+    @Override
+    public void deleteById(Long id) {
+
+    }
+}
